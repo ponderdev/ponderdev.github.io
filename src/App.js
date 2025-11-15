92158ba63f48a0fee0381978e83dfa5e7f695dce
GlTHUB-user <noahdeyck4@gmail.com>
2025-11-15 20:58:59 +0000

    initial comit

--- src/App.js
+++ src/App.js
@@ -6,18 +6,45 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>
+          Hi, I am Noah
+        </h3>
+
+        <small>
+          though you may know me as ponderdev, or ponderer from Discord or Github
+        </small>
+
+        <h1>
+          I am interested in:
+        </h1>
+
+        <ul>
+          <li>Cybersecurity</li>
+          <li>iOS development</li>
+          <li>Web development</li>
+          <li>iOS jailbreaking</li>
+        </ul>
+
+        <h1>
+          Join my small Discord server:
+        </h1>
+
+        <a className='App-invite' href="https://discord.gg/gQ4vRy2GEr">.gg/gQ4vRy2GEr</a>
+
+        <h1>
+          My projects:
+        </h1>
+
+        <ul>
+          <li>This site you're on</li>
+          <li>Sideload-Manager</li>
+        </ul>
+
       </header>
+
+      <footer className="App-footer">
+        © 2025 Sandbox Escape. All rights reserved.
+      </footer>
     </div>
   );
 }
